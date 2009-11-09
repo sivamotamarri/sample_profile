0b2bc900fb8b4bac4b8d85bbbd03243d1a91ddb3
sivakrishna <sivakrishna.motamarri@gmail.com>
2009-11-09 09:46:49 +0530

    app was added

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+  
+def authorize
+  @user = Account.find_by_id(session[:user_id]) 
+  if @user.blank?
+  flash[:error] = "Please log in"
+  redirect_to :controller => 'login'  
+  end
+end
+end
---
+++ app/models/friend_email.rb
@@ -0,0 +1,16 @@
+class FriendEmail < ActionMailer::Base
+  
+  def to_friend(to,subject,body,name)
+    
+    
+    @recipients = to
+    @from = "myprofile.com"
+    @subject = subject  
+   
+    @body["body"] = body
+    @body["username"] = name
+    
+
+ end
+
+end
